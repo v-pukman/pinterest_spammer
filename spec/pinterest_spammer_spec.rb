06f8a664433b0865d465ed08156f9307c6bd3e3d
Victor Pukman <victor.pukman@gmail.com>
2016-02-10 21:58:52 +0800

    refactor hash style in tests

--- spec/pinterest_spammer_spec.rb
+++ spec/pinterest_spammer_spec.rb
@@ -6,7 +6,7 @@ describe PinterestSpammer do
 
   describe '#sign_in' do
     it 'sign in to Pinterest using username and password' do
-      stub_request(:post, 'https://www.pinterest.com/resource/UserSessionResource/create/').to_return(:status => 200, :body => "", :headers => {})
+      stub_request(:post, 'https://www.pinterest.com/resource/UserSessionResource/create/').to_return(status: 200, body: "", headers: {})
       result = agent.sign_in 'username_test', 'password_test'
       expect(result[:success]).to eq true
     end
@@ -23,7 +23,7 @@ describe PinterestSpammer do
       allow(cookie).to receive(:name).and_return('csrftoken')
       allow(cookie).to receive(:value).and_return('1234')
       allow(agent.agent).to receive(:cookies).and_return([cookie])
-      stub_request(:post, 'https://www.pinterest.com/resource/UserSessionResource/create/').to_return(:status => 200, :body => "", :headers => {})
+      stub_request(:post, 'https://www.pinterest.com/resource/UserSessionResource/create/').to_return(status: 200, body: "", headers: {})
       result = agent.sign_in 'username_test', 'password_test'
       expect(agent.csrftoken).to eq '1234'
     end
@@ -35,7 +35,7 @@ describe PinterestSpammer do
     let(:image_url) { 'http://rubyonrails.org/images/rails.png' }
     let(:description) { 'Spammer!' }
     it 'creates a pin' do
-      stub_request(:post, 'https://www.pinterest.com/resource/PinResource/create/').to_return(:status => 200, :body => { resource_response: { data: { id: 1 }}}.to_json, :headers => {})
+      stub_request(:post, 'https://www.pinterest.com/resource/PinResource/create/').to_return(status: 200, body: { resource_response: { data: { id: 1 }}}.to_json, headers: {})
       result = agent.create_pin(board_id, link, image_url, description)
       expect(result[:success]).to eq true
       expect(result[:pin_id]).to_not eq nil
