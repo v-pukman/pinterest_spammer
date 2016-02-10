c808b29a400bf8682da7955a397d62feeb7b7150
Victor Pukman <victor.pukman@gmail.com>
2016-02-10 21:33:21 +0800

    create_pin error test added

--- spec/pinterest_spammer_spec.rb
+++ spec/pinterest_spammer_spec.rb
@@ -29,14 +29,23 @@ describe PinterestSpammer do
     end
   end
 
-  it 'creates a pin' do
-    stub_request(:post, 'https://www.pinterest.com/resource/PinResource/create/').to_return(:status => 200, :body => { resource_response: { data: { id: 1 }}}.to_json, :headers => {})
-    board_id = 1234
-    link = 'https://xyz.xyz/'
-    image_url = 'http://rubyonrails.org/images/rails.png'
-    description = 'Spammer!'
-    result = agent.create_pin(board_id, link, image_url, description)
-    expect(result[:success]).to eq true
-    expect(result[:pin_id]).to_not eq nil
+  describe '#create_pin' do
+    let(:board_id) { 1234 }
+    let(:link) { 'https://xyz.xyz/' }
+    let(:image_url) { 'http://rubyonrails.org/images/rails.png' }
+    let(:description) { 'Spammer!' }
+    it 'creates a pin' do
+      stub_request(:post, 'https://www.pinterest.com/resource/PinResource/create/').to_return(:status => 200, :body => { resource_response: { data: { id: 1 }}}.to_json, :headers => {})
+      result = agent.create_pin(board_id, link, image_url, description)
+      expect(result[:success]).to eq true
+      expect(result[:pin_id]).to_not eq nil
+    end
+
+    it 'return error message on fail' do
+      allow(agent).to receive(:default_headers) { raise StandardError }
+      result = agent.create_pin(board_id, link, image_url, description)
+      expect(result[:success]).to eq false
+      expect(result[:error_msg]).to_not eq nil
+    end
   end
 end
