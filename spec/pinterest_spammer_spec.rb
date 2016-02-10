77d980819ddb45273e2192f294ffd27c3089d88f
Victor Pukman <victor.pukman@gmail.com>
2016-02-10 15:41:39 +0800

    sign_in test refactored

--- spec/pinterest_spammer_spec.rb
+++ spec/pinterest_spammer_spec.rb
@@ -7,7 +7,7 @@ describe PinterestSpammer do
   it 'sign in to Pinterest using username and password' do
     stub_request(:post, 'https://www.pinterest.com/resource/UserSessionResource/create/').to_return(:status => 200, :body => "", :headers => {})
     result = agent.sign_in 'username_test', 'password_test'
-    expect(result['status']).to eq 'success'
+    expect(result[:success]).to eq true
   end
 
   it 'creates a pin'
