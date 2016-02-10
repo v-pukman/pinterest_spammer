8777d3d31bb4d05e9d7ac514a148e7b246a1a721
Victor Pukman <victor.pukman@gmail.com>
2016-02-10 14:48:47 +0800

    sign_in test added

--- spec/pinterest_spammer_spec.rb
+++ spec/pinterest_spammer_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 require 'pinterest_spammer'
 
 describe PinterestSpammer do
-  it 'sign in to Pinterest using username and password'
+  let(:agent) { PinterestSpammer.new }
+
+  it 'sign in to Pinterest using username and password' do
+    result = agent.sign_in 'username_test', 'password_test'
+    expect(result['status']).to eq 'success'
+  end
+
   it 'creates a pin'
 end
